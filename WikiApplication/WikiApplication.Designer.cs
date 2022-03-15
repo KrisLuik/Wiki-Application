94974da248aeac9f78bd2320e59d994e3c710826
Kris <kristribbeck@gmail.com>
2022-03-15 11:18:12 +0800

    Add, Delete, Listview

---
+++ WikiApplication/WikiApplication.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WikiApplication
+{
+    partial class WikiApplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.textBoxStructure = new System.Windows.Forms.TextBox();
+            this.textBoxDefinition = new System.Windows.Forms.TextBox();
+            this.listViewDataStructures = new System.Windows.Forms.ListView();
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnCategory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 22);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 33);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(12, 66);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(232, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Location = new System.Drawing.Point(12, 96);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(232, 20);
+            this.textBoxCategory.TabIndex = 2;
+            // 
+            // textBoxStructure
+            // 
+            this.textBoxStructure.Location = new System.Drawing.Point(12, 126);
+            this.textBoxStructure.Name = "textBoxStructure";
+            this.textBoxStructure.Size = new System.Drawing.Size(232, 20);
+            this.textBoxStructure.TabIndex = 3;
+            // 
+            // textBoxDefinition
+            // 
+            this.textBoxDefinition.Location = new System.Drawing.Point(12, 160);
+            this.textBoxDefinition.Multiline = true;
+            this.textBoxDefinition.Name = "textBoxDefinition";
+            this.textBoxDefinition.Size = new System.Drawing.Size(232, 218);
+            this.textBoxDefinition.TabIndex = 4;
+            // 
+            // listViewDataStructures
+            // 
+            this.listViewDataStructures.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listViewDataStructures.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnName,
+            this.columnCategory});
+            this.listViewDataStructures.HideSelection = false;
+            this.listViewDataStructures.Location = new System.Drawing.Point(282, 96);
+            this.listViewDataStructures.Name = "listViewDataStructures";
+            this.listViewDataStructures.Size = new System.Drawing.Size(358, 294);
+            this.listViewDataStructures.TabIndex = 5;
+            this.listViewDataStructures.UseCompatibleStateImageBehavior = false;
+            this.listViewDataStructures.View = System.Windows.Forms.View.Details;
+            this.listViewDataStructures.Click += new System.EventHandler(this.ListViewDataStructures_Click);
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            this.columnName.Width = 151;
+            // 
+            // columnCategory
+            // 
+            this.columnCategory.Text = "Category";
+            this.columnCategory.Width = 199;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(93, 22);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 33);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // WikiApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 429);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.listViewDataStructures);
+            this.Controls.Add(this.textBoxDefinition);
+            this.Controls.Add(this.textBoxStructure);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "WikiApplication";
+            this.Text = "Wiki Data Structures";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.TextBox textBoxStructure;
+        private System.Windows.Forms.TextBox textBoxDefinition;
+        private System.Windows.Forms.ListView listViewDataStructures;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnCategory;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ WikiApplication/WikiApplication.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WikiApplication
+{
+    public partial class WikiApplication : Form
+    {
+        public WikiApplication()
+        {
+            InitializeComponent();
+        }
+        #region Properties
+        static int rowSize = 12;
+        static int colSize = 4; // Name, Category, Structure, Definition
+        string[,] dataStructures = new string[rowSize, colSize];
+        string defaultFileName = "default.bin";
+        int ptr = 0;
+        #endregion
+        #region ListView
+        private void ListViewDataStructures_Click(object sender, EventArgs e)
+        {
+            int currentDataStructure = listViewDataStructures.SelectedIndices[0];
+            textBoxName.Text = dataStructures[currentDataStructure, 0 ];
+            textBoxCategory.Text = dataStructures[currentDataStructure, 1];
+            textBoxStructure.Text = dataStructures[currentDataStructure, 2];
+            textBoxDefinition.Text = dataStructures[currentDataStructure, 3];
+        }
+        #endregion
+        #region Display Data Structures
+        private void DisplayDataStructures()
+        {
+            listViewDataStructures.Items.Clear();
+            listViewDataStructures.ForeColor = Color.Black;
+            for (int x = 0; x < rowSize; x++)
+            {
+                // Displaying name and category
+                ListViewItem listviewItem = new ListViewItem(dataStructures[x, 0]);
+                listviewItem.SubItems.Add(dataStructures[x, 1]);
+                listviewItem.SubItems.Add(dataStructures[x, 2]);
+                listViewDataStructures.Items.Add(listviewItem);
+            }
+        }
+        #endregion
+        #region Clear TextBoxes Method
+        private void ClearTextBoxes()
+        {
+            textBoxName.Clear();
+            textBoxName.Focus();
+
+            textBoxCategory.Clear();
+            textBoxCategory.Focus();
+
+            textBoxStructure.Clear();
+            textBoxStructure.Focus();
+
+            textBoxDefinition.Clear();
+            textBoxDefinition.Focus();
+        }
+        #endregion
+        #region Add button
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxName.Text) &&
+               !string.IsNullOrWhiteSpace(textBoxCategory.Text) &&
+               !string.IsNullOrWhiteSpace(textBoxStructure.Text) &&
+               !string.IsNullOrWhiteSpace(textBoxDefinition.Text))
+            {
+                for (int x = 0; x < rowSize; x++)
+                {
+                    if (ptr < rowSize)
+                    {
+                        dataStructures[x, 0] = textBoxName.Text;
+                        dataStructures[x, 1] = textBoxCategory.Text;
+                        dataStructures[x, 2] = textBoxStructure.Text;
+                        dataStructures[x, 3] = textBoxDefinition.Text;
+
+                        var result = MessageBox.Show("Proceed with new definition?", "Add New Definition",
+                            MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                        if (result == DialogResult.OK)
+                            break;
+                        else
+                        {
+                            dataStructures[x, 0] = "";
+                            dataStructures[x, 1] = "Category";
+                            dataStructures[x, 2] = "Structure";
+                            dataStructures[x, 3] = "Definition";
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            DisplayDataStructures();
+            ClearTextBoxes();
+        }
+        #endregion
+        #region Delete Button
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int currentRecord = listViewDataStructures.SelectedIndices[0];
+            if (currentRecord >= 0)
+            {
+                DialogResult delName = MessageBox.Show("Do you wish to delete this Name?",
+                 "Delete Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (delName == DialogResult.Yes)
+                {
+                    dataStructures[currentRecord, 0] = "";
+                    dataStructures[currentRecord, 1] = "";
+                    dataStructures[currentRecord, 2] = "";
+                    dataStructures[currentRecord, 3] = "";
+                    DisplayDataStructures();
+                    ClearTextBoxes();
+                    
+                }
+            }
+        }
+        #endregion
+    }
+}
