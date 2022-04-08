9006c257f7fd428cc5144255b6a4f4560a6837c3
KrisTribbeck <kristribbeck@gmail.com>
2022-04-09 02:13:20 +0800

    Error trapping and toolstrip messages

--- WikiApplication/WikiApplication.cs
+++ WikiApplication/WikiApplication.cs
@@ -52,6 +52,7 @@ namespace WikiApplication
         #region Display Data Structures
         private void DisplayArray()
         {
+            ClearStatusMessage();
             listViewDataStructures.Items.Clear();
             listViewDataStructures.ForeColor = Color.Black;
             for (int x = 0; x < rowSize; x++)
@@ -70,10 +71,9 @@ namespace WikiApplication
         #endregion
         // Create an ADD button that will store the information from the 4 text boxes into the 2D array.
         #region Add button
-
         private void ButtonAdd_Click(object sender, EventArgs e)
-            // Needs else if statement/ or toolstrip message when user hasn't added anything to textboxes 
-            // but clicks add button.
+        // Needs else if statement/ or toolstrip message when user hasn't added anything to textboxes 
+        // but clicks add button.
         {
             ClearStatusMessage();
             if (!string.IsNullOrWhiteSpace(textBoxName.Text) &&
@@ -109,12 +109,29 @@ namespace WikiApplication
                     }
                 }
             }
+            else
+            {
+                toolStripStatusLabel1.Text = "Error: Text boxes are empty.";
+                textBoxName.Focus();
+            }
         }
         #endregion
         // Create a DELETE button that will clear the selected item from the listviewbox.
         #region Delete Button
-
         private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            Delete();
+            if (!string.IsNullOrWhiteSpace(textBoxName.Text) &&
+                !string.IsNullOrWhiteSpace(textBoxCategory.Text) &&
+                !string.IsNullOrWhiteSpace(textBoxStructure.Text) &&
+                !string.IsNullOrWhiteSpace(textBoxDefinition.Text))
+            {
+                ClearTextBoxes();
+                toolStripStatusLabel1.Text = "Text boxes cleared.";
+            }
+        }
+
+        private void Delete()
         {
             try
             {
@@ -146,7 +163,8 @@ namespace WikiApplication
             }
             catch (ArgumentOutOfRangeException)
             {
-                MessageBox.Show("Select an item to delete.");
+                ClearStatusMessage();
+                toolStripStatusLabel1.Text = "Error: Not a valid action. ";
             }
         }
         #endregion
@@ -170,13 +188,19 @@ namespace WikiApplication
                         dataStructures[currentRecord, 3] = textBoxDefinition.Text;
                         DisplayArray();
                         ClearTextBoxes();
-                        toolStripStatusLabel1.Text = "Data item changed.";
+                        toolStripStatusLabel1.Text = "Data Item changed.";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Item NOT changed", "Edit Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        toolStripStatusLabel1.Text = "Data item not changed. ";
                     }
                 }
             }
             catch (ArgumentOutOfRangeException)
             {
                 MessageBox.Show("Please select an item to edit.");
+                textBoxName.Focus();
             }
         }
         #endregion
@@ -193,64 +217,72 @@ namespace WikiApplication
             int finalIndex = ptr;
             bool flag = false;
             int foundIndex = -1;
-
-            while (!flag && !((finalIndex - startIndex) <= 1))
+            try
             {
-                int newIndex = (finalIndex + startIndex) / 2;
-                if (string.Compare(dataStructures[newIndex, 0], textBoxSearch.Text) == 0)
+                while (!flag && !((finalIndex - startIndex) <= 1))
                 {
-                    foundIndex = newIndex;
-                    flag = true;
-                    listViewDataStructures.Items[0].Selected = true;
-                    toolStripStatusLabel1.Text = "Found at index " + foundIndex;
-                    break;
+                    int newIndex = (finalIndex + startIndex) / 2;
+
+                    string userInput = textBoxSearch.Text = textBoxSearch.Text.Substring(0, 1).ToUpper() + textBoxSearch.Text.Substring(1);
+                    if (userInput.Length >= 1)
+                    {
+                        if (string.Compare(dataStructures[newIndex, 0], textBoxSearch.Text) == 0)
+                        {
+                            foundIndex = newIndex;
+                            flag = true;
+                            listViewDataStructures.Items[0].Selected = true;
+                            toolStripStatusLabel1.Text = "Found at index " + foundIndex;
+                            break;
+                        }
+                        else
+                        {
+                            if (string.Compare(dataStructures[newIndex, 0], textBoxSearch.Text) == 1)
+                            {
+                                finalIndex = newIndex;
+                            }
+                            else
+                                startIndex = newIndex;
+                        }
+                    }
+                }
+                if (flag)
+                {
+                    textBoxSearch.Text = dataStructures[foundIndex, 0];
+                    textBoxName.Text = dataStructures[foundIndex, 0];
+                    textBoxCategory.Text = dataStructures[foundIndex, 1];
+                    textBoxStructure.Text = dataStructures[foundIndex, 2];
+                    textBoxDefinition.Text = dataStructures[foundIndex, 3];
+                }
+                else if (string.IsNullOrWhiteSpace(textBoxSearch.Text))
+                {
+                    toolStripStatusLabel1.Text = "Enter a name to search.";
+                    ClearSearchBox();
                 }
                 else
                 {
-                    if (string.Compare(dataStructures[newIndex, 0], textBoxSearch.Text) == 1)
-                    {
-                        finalIndex = newIndex;
-                    }
-                    else
-                        startIndex = newIndex;
+                    toolStripStatusLabel1.Text = "Not Found.";
+                    ClearSearchBox();
                 }
             }
-            if (flag)
+            catch (ArgumentOutOfRangeException)
             {
-                textBoxSearch.Text = dataStructures[foundIndex, 0];
-                textBoxName.Text = dataStructures[foundIndex, 0];
-                textBoxCategory.Text = dataStructures[foundIndex, 1];
-                textBoxStructure.Text = dataStructures[foundIndex, 2];
-                textBoxDefinition.Text = dataStructures[foundIndex, 3];
+                MessageBox.Show("Error! Use search textbox to find an item.");
+                textBoxSearch.Focus();
             }
-            else if (string.IsNullOrWhiteSpace(textBoxSearch.Text))
-            {
-                toolStripStatusLabel1.Text = "Enter a name to search.";
-                ClearSearchBox();
-            }
-            else if (!string.IsNullOrWhiteSpace(textBoxSearch.Text) || listViewDataStructures.Items.Count == -1)
-            {
-                toolStripStatusLabel1.Text = "Error: Add data to list first.";
-                ClearSearchBox();
-            }
-            else
-            {
-                toolStripStatusLabel1.Text = "Not Found.";
-                ClearSearchBox();
-            }
+            
         }
         #endregion
         // Write the code for a Bubble Sort method to sort the 2D array by Name ascending.
         #region Bubble sort 
         private void Sort()
         {
-            for (int x = 0; x < rowSize-1; x++)
+            for (int x = 0; x < rowSize - 1; x++)
             {
                 for (int i = 0; i < rowSize - 1; i++)
                 {
                     if (!(string.IsNullOrEmpty(dataStructures[i + 1, 0])))
                     {
-                        if (string.CompareOrdinal(dataStructures[i, 0], dataStructures[i + 1, 1]) > 0)
+                        if (string.CompareOrdinal(dataStructures[i, 0], dataStructures[i + 1, 0]) > 0)
                         {
                             Swap(i);
                         }
@@ -275,14 +307,17 @@ namespace WikiApplication
         // A double mouse click in the search text box will clear the search input box.
         #region Double-click Clear Searchbox
         private void TextBoxSearch_MouseDoubleClick(object sender, MouseEventArgs e)
-
         {
+            ClearStatusMessage();
             if (!string.IsNullOrWhiteSpace(textBoxSearch.Text))
             {
-                textBoxSearch.Clear();
+                ClearSearchBox();
                 ClearTextBoxes();
-                textBoxSearch.Focus();
-                ClearStatusMessage();
+                toolStripStatusLabel1.Text = "Search textbox and text boxes cleared.";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Search box is empty!";
             }
         }
         #endregion
@@ -290,22 +325,23 @@ namespace WikiApplication
         #region Form Load
         private void WikiApplication_Load(object sender, EventArgs e)
         {
-            toolStripStatusLabel1.Text = "Wiki Application for Data Structures.";
-            for (int x = 0; x < rowSize ; x++)
+
+            for (int x = 0; x < rowSize; x++)
             {
                 for (int y = 0; y < colSize - 1; y++)
                 {
                     dataStructures[x, y] = "~";
                 }
             }
-            Sort();
             DisplayArray();
+            toolStripStatusLabel1.Text = "Wiki Application for Data Structures.";
         }
         #endregion
         // Create a LOAD/OPEN button that will read the information from a binary file called definitions.dat into the 2D array.
         #region Open 
         private void ButtonOpen_Click(object sender, EventArgs e)
         {
+            ClearStatusMessage();
             BinaryReader br;
             int x = 0;
             listViewDataStructures.Items.Clear();
@@ -376,25 +412,25 @@ namespace WikiApplication
         #region TEST
         private void buttonTest_Click(object sender, EventArgs e)
         {
-            dataStructures[0, 0] = "nameone";
-            dataStructures[1, 0] = "nametwo";
-            dataStructures[2, 0] = "namethree";
-            dataStructures[3, 0] = "namefour";
+            dataStructures[0, 0] = "January";
+            dataStructures[1, 0] = "February";
+            dataStructures[2, 0] = "March";
+            dataStructures[3, 0] = "April";
 
-            dataStructures[0, 1] = "categoryone";
-            dataStructures[1, 1] = "categorytwo";
-            dataStructures[2, 1] = "categorythree";
-            dataStructures[3, 1] = "categoryfour";
+            dataStructures[0, 1] = "May";
+            dataStructures[1, 1] = "June";
+            dataStructures[2, 1] = "July";
+            dataStructures[3, 1] = "August";
 
-            dataStructures[0, 2] = "structureone";
-            dataStructures[1, 2] = "structureetwo";
-            dataStructures[2, 2] = "structurethree";
-            dataStructures[3, 2] = "structurefour";
+            dataStructures[0, 2] = "September";
+            dataStructures[1, 2] = "October";
+            dataStructures[2, 2] = "November";
+            dataStructures[3, 2] = "December";
 
-            dataStructures[0, 3] = "definitionone";
-            dataStructures[1, 3] = "definitiontwo";
-            dataStructures[2, 3] = "definitionthree";
-            dataStructures[3, 3] = "definitionfour";
+            dataStructures[0, 3] = "Monday";
+            dataStructures[1, 3] = "Tuesday";
+            dataStructures[2, 3] = "Wednesday";
+            dataStructures[3, 3] = "Thursday";
             ptr += 4;
             DisplayArray();
         }
@@ -414,24 +450,24 @@ namespace WikiApplication
                 e.Handled = true;
             }
         }
-        private void textBoxName_KeyPress(object sender, KeyPressEventArgs e)
+        private void TextBoxName_KeyPress(object sender, KeyPressEventArgs e)
         {
             InputHandling(e);
         }
 
-        private void textBoxCategory_KeyPress(object sender, KeyPressEventArgs e)
+        private void TextBoxCategory_KeyPress(object sender, KeyPressEventArgs e)
         {
             InputHandling(e);
         }
 
-        private void textBoxStructure_KeyPress(object sender, KeyPressEventArgs e)
+        private void TextBoxStructure_KeyPress(object sender, KeyPressEventArgs e)
         {
             InputHandling(e);
         }
 
-        private void textBoxDefinition_KeyPress(object sender, KeyPressEventArgs e)
+        private void TextBoxDefinition_KeyPress(object sender, KeyPressEventArgs e)
         {
-            //InputHandling(e);
+            InputHandling(e);
         }
         #endregion
         // Create a CLEAR method to clear the four text boxes so a new definition can be added.
@@ -455,12 +491,22 @@ namespace WikiApplication
             textBoxStructure.Clear();
             textBoxDefinition.Clear();
         }
-        #endregion
-
-        private void buttonSort_Click(object sender, EventArgs e)
+        // Clear status message everytime the form is clicked.
+        private void WikiApplication_Click(object sender, EventArgs e)
+        {
+            ClearStatusMessage();
+            textBoxName.Focus();
+        }
+        private void ButtonSort_Click(object sender, EventArgs e)
         {
             Sort();
             DisplayArray();
         }
+
+        private void TextBoxName_Click(object sender, EventArgs e)
+        {
+            ClearStatusMessage();
+        }
     }
+    #endregion
 }
